642a6c976a3273a016df658825d5b11374c216d8
Noah D. Brenowitz <nbren12@gmail.com>
2015-05-07 17:32:55 -0400

    outputing solutions for various times

--- src/twodheat.cpp
+++ src/twodheat.cpp
@@ -1,32 +1,58 @@
 #include <cmath>
+#include <iostream>
+#include <cstdio>
 
 #include "util.hpp"
 #include "laplace.h"
 
+using namespace std;
 
 
 void evolve_heat_equation_2d(double *x, int n, const double tout,
 			     const double dx, const double dt_min,
+			     int output_interval,
 			     double* work){
 
   int i;
-  double lambda = dt_min / dx / dx;
 
+  // Setup time stepping
+  const int nt = ceil(tout/dt_min);
+  const double dt = tout / nt;
+  const double lambda = dt_min / dx / dx;
+
+  cout << "Running heat solver" << endl;
+  cout << "Total time = " << tout << endl;
+  cout << "dt = " << dt << endl;
+  cout << "num timesteps = " << nt << endl;
 
   setup_laplacian(n,n);
   set_lambda_cn(lambda);
 
+  int it;
 
-  // forward step
-  apply_laplacian(work, x);
-  for (i = 0; i < (n+2)*(n+2); i++) {
-    x[i] += work[i]/2.0;
-    work[i] = x[i]; // copy back into work array
+  char output_filename[100];
+  int count=0;
+  
+  for (it = 1; it < nt+1; it++) {
+
+    if (it%output_interval == 0){
+      sprintf(output_filename, "%d.txt", count++);
+      cout << it * dt << " " << output_filename << endl;
+      print_state(output_filename, n, n, x);
+      
+    }
+    // forward step
+    apply_laplacian(work, x);
+    for (i = 0; i < (n+2)*(n+2); i++) {
+      x[i] += work[i]/2.0;
+      work[i] = x[i]; // copy back into work array
+    }
+
+    // backward step
+    backward_solve(x, work);
   }
 
-  // backward step
-  backward_solve(x, work);
-  
+  free_solvers();
 }
 
 
@@ -64,10 +90,8 @@ int test_evolve_heat_equation_2d(){
   // Solve heat equation
   
   print_state("t0.txt", nx,ny, x0);
-  evolve_heat_equation_2d(x0, nx, dt_outer, dx, dt_inner, x);
+  evolve_heat_equation_2d(x0, nx, dt_outer, dx, dt_inner, 2, x);
   print_state("t1.txt", nx,ny, x0);
-
-  free_solvers();
 
   free(x);
   free(x0);
